7791fb9a460ec06e226d477a77ac0568230856f2
gabdanho <bogdanbabenko201804@gmail.com>
2025-08-14 17:31:49 +0300

    add common error class in FirebaseResult

---
+++ app/src/main/java/com/example/autohub/presentation/model/LoadingState.kt
@@ -0,0 +1,10 @@
+package com.example.autohub.presentation.model
+
+sealed class LoadingState {
+
+    data object Loading : LoadingState()
+
+    data object Success : LoadingState()
+
+    data class Error(val message: String?) : LoadingState()
+}
