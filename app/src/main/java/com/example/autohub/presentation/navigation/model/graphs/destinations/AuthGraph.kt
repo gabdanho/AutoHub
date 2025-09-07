f62812f5463e19c19d42d2b416226874d618062c
Bogdan <bogdanbabenko201804@gmail.com>
2025-09-07 20:57:38 +0300

    implement NavigationDestination

--- app/src/main/java/com/example/autohub/presentation/navigation/model/graphs/destinations/AuthGraph.kt
+++ app/src/main/java/com/example/autohub/presentation/navigation/model/graphs/destinations/AuthGraph.kt
@@ -3,7 +3,7 @@ package com.example.autohub.presentation.navigation.model.graphs.destinations
 import kotlinx.serialization.Serializable
 
 @Serializable
-sealed interface AuthGraph {
+sealed interface AuthGraph : NavigationDestination {
 
     @Serializable
     data object LoginScreen : AuthGraph
