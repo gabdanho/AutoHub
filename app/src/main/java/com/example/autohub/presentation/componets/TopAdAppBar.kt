c25ba28e735909f9e7909f6e2d0a0f55d6b25049
Bogdan <bogdanbabenko201804@gmail.com>
2025-09-10 19:36:22 +0300

    fix button padding

--- app/src/main/java/com/example/autohub/presentation/componets/TopAdAppBar.kt
+++ app/src/main/java/com/example/autohub/presentation/componets/TopAdAppBar.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -23,7 +24,10 @@ fun TopAdAppBar(
         verticalAlignment = Alignment.CenterVertically,
         modifier = modifier
     ) {
-        BackButton(onBackClick = { onBackButtonClick() })
+        BackButton(
+            onBackClick = { onBackButtonClick() },
+            modifier = Modifier.padding(8.dp)
+        )
         Text(
             text = titleText,
             style = MaterialTheme.typography.titleLarge,
